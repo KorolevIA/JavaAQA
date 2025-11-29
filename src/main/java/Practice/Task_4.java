e2a0cbe28a4aad03ddb35a20d827416ae907af46
Илья <korolev_ilia@mail.ru>
2025-11-29 15:13:39 +0300

    Поменял исключение в Task_4

--- src/main/java/Practice/Task_4.java
+++ src/main/java/Practice/Task_4.java
@@ -6,13 +6,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Task_4 {
 
-    static void main() throws InterruptedException {
+    static void main() {
 
         WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("http://the-internet.herokuapp.com/entry_ad");
 
-        Thread.sleep(1000);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         driver.findElement(By.cssSelector(".modal-footer p")).click();
         String contentText = driver.findElement(By.cssSelector("#content")).getText();
